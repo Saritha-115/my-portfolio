193758b237dd96eedeb8dd1dbc9035352a8191e8
Saritha Themiyadasa <srththemiyadasa@gmail.com>
2025-05-11 19:27:05 +0530

    styling changes

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,8 +1,10 @@
 export default function Footer() {
   return (
     <footer className="py-4">
-      <div className="container text-center">
-        <p>&copy; {new Date().getFullYear()} My Portfolio. All rights reserved.</p>
+      <div className="container mx-auto text-center">
+        <p>
+          &copy; {new Date().getFullYear()} My Portfolio. All rights reserved.
+        </p>
       </div>
     </footer>
   );
